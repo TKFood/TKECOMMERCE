5092a2ada9bb05b020d9e21ed231145e3d35a75c
at3158 <tk290@tkfood.com.tw>
2016-07-13 15:44:30 +0800

    login

---
+++ TKECOMMERCE/FrmParent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Reflection;
+
+namespace TKECOMMERCE
+{
+    public partial class FrmParent : Form
+    {
+        SqlConnection conn;
+        MenuStrip MnuStrip;
+        ToolStripMenuItem MnuStripItem;
+        string UserName;
+
+        public FrmParent()
+        {
+            InitializeComponent();
+        }
+
+        public FrmParent(string txt_UserName)
+        {
+            InitializeComponent();
+            UserName = txt_UserName;
+        }
+    }
+}
