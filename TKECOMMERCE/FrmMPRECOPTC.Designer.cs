3927a837356ce077296da6147090f3f8f38841a1
at3158 <tk290@tkfood.com.tw>
2016-07-21 11:56:09 +0800

    excel import

--- TKECOMMERCE/FrmMPRECOPTC.Designer.cs
+++ TKECOMMERCE/FrmMPRECOPTC.Designer.cs
@@ -28,19 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // FrmPRECOPTC
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.LightGreen;
+            this.tableLayoutPanel1.ColumnCount = 11;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 10);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 11;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(83, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(74, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 9);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 6);
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(714, 234);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(83, 363);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(154, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 403);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FrmMPRECOPTC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Name = "FrmPRECOPTC";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmMPRECOPTC";
             this.Text = "FrmPRECOPTC";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- TKECOMMERCE/FrmMPRECOPTC.cs
+++ TKECOMMERCE/FrmMPRECOPTC.cs
@@ -7,14 +7,221 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NPOI;
+using NPOI.HPSF;
+using NPOI.HSSF;
+using NPOI.HSSF.UserModel;
+using NPOI.POIFS;
+using NPOI.Util;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.Extractor;
+using System.IO;
+using System.Data.SqlClient;
+using NPOI.SS.UserModel;
+using System.Configuration;
+using NPOI.XSSF.UserModel;
 
 namespace TKECOMMERCE
 {
     public partial class FrmMPRECOPTC : Form
     {
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlComm = new SqlCommand();
+        string connectionString;
+        StringBuilder sbSql = new StringBuilder();
+        StringBuilder sbSqlQuery = new StringBuilder();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        SqlCommandBuilder sqlCmdBuilder = new SqlCommandBuilder();
+        SqlTransaction tran;
+        SqlCommand cmd = new SqlCommand();
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        string strFilePath;
+        OpenFileDialog file = new OpenFileDialog();
+        int result;
+        string NowDay;
+        string NowDB = "test";
+
         public FrmMPRECOPTC()
         {
             InitializeComponent();
+            SetMyCustomFormat();
         }
+
+        #region FUNCTION
+        public void SetMyCustomFormat()
+        {
+            // Set the Format type and the CustomFormat string.
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy - MM";
+        }
+
+        public void Search()
+        {
+            try
+            {
+
+                if (!string.IsNullOrEmpty(dateTimePicker1.Text.ToString()) )
+                {
+                    connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                    sqlConn = new SqlConnection(connectionString);
+
+                    sbSql.Clear();
+                    sbSqlQuery.Clear();
+
+                    sbSqlQuery.AppendFormat("{0}",dateTimePicker1.Value.ToString("yyyyMM"));
+                    sbSql.AppendFormat(@"SELECT  YEARMONTH AS '年月', ZTKECOMMERCEFrmMPRECOPTC.MB001 AS '品號', ZTKECOMMERCEFrmMPRECOPTC.MB002 AS '品名', PREOrderNum AS '數量',MB004 AS '單位' FROM  [{0}].[dbo].ZTKECOMMERCEFrmMPRECOPTC  WITH (NOLOCK) ,[{1}].[dbo].INVMB  WITH (NOLOCK) WHERE ZTKECOMMERCEFrmMPRECOPTC.MB001=INVMB.MB001 AND YEARMONTH='{2}'  ", NowDB, NowDB, sbSqlQuery.ToString());
+
+                    adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+                    sqlCmdBuilder = new SqlCommandBuilder(adapter);
+
+                    sqlConn.Open();
+                    ds.Clear();
+                    adapter.Fill(ds, "TEMPds");
+                    sqlConn.Close();
+
+
+                    if (ds.Tables["TEMPds"].Rows.Count == 0)
+                    {
+                        
+                    }
+                    else
+                    {                       
+
+                        dataGridView1.DataSource = ds.Tables["TEMPds"];
+                        dataGridView1.AutoResizeColumns();
+                    }
+                }
+                else
+                {
+
+                }
+
+
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+
+        public void OpenFile()
+        {            
+            file.ShowDialog();
+            file.Filter =
+                "請開啟EXCEL檔 files (*XlSX" +
+                 "All Files (*.*)|*.*";
+            file.Title = "請開啟EXCEL檔";
+
+            strFilePath = file.FileName.ToString();
+            textBox1.Text = file.SafeFileName;      
+
+        }
+
+        public void ExcelImport()
+        {
+            try
+            {
+                IWorkbook workbook;
+                using (FileStream stream = new FileStream(strFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    workbook = new XSSFWorkbook(stream);
+                }
+
+                ISheet sheet = workbook.GetSheetAt(0); // zero-based index of your target sheet
+                dt = new DataTable(sheet.SheetName);
+
+                // write header row
+                IRow headerRow = sheet.GetRow(0);
+                foreach (ICell headerCell in headerRow)
+                {
+                    dt.Columns.Add(headerCell.ToString());
+                }
+
+                // write the rest
+                int rowIndex = 0;
+                foreach (IRow row in sheet)
+                {
+                    // skip header row
+                    if (rowIndex++ == 0) continue;
+                    DataRow dataRow = dt.NewRow();
+                    dataRow.ItemArray = row.Cells.Select(c => c.ToString()).ToArray();
+                    dt.Rows.Add(dataRow);
+                }
+
+          
+                //add ZTKECOMMERCEFrmMPRECOPTC
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+                sbSql.Append(" ");               
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    sbSql.AppendFormat(" INSERT INTO [{0}].[dbo].[ZTKECOMMERCEFrmMPRECOPTC] ", NowDB);
+                    sbSql.Append(" (YEARMONTH, MB001,MB002,  PREOrderNum )");
+                    sbSql.AppendFormat(" VALUES ('{0}','{1}','NA','{2}')", dt.Rows[i]["年月"].ToString(), dt.Rows[i]["品號"].ToString(), dt.Rows[i]["數量"].ToString());
+                    sbSql.Append(" ");
+                }
+                sbSql.AppendFormat(" UPDATE [{0}].[dbo].[ZTKECOMMERCEFrmMPRECOPTC] SET ZTKECOMMERCEFrmMPRECOPTC.MB002=INVMB.MB002 FROM [{1}].[dbo].[INVMB] WHERE ZTKECOMMERCEFrmMPRECOPTC.MB001=INVMB.MB001 AND ZTKECOMMERCEFrmMPRECOPTC.MB002='NA'", NowDB, NowDB);
+                sbSql.Append(" ");
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易                    
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+            //dataGridView1.DataSource = dt;
+            Search();
+        }
+
+
+        #endregion
+
+        #region BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExcelImport();
+        }
+        #endregion
+
+
     }
 }
