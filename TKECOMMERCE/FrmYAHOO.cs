71cf3e3b5622e8131f6adb90940b82f5bdb498ea
at3158 <tk290@tkfood.com.tw>
2017-12-01 16:20:43 +0800

    Bulk_Insert

--- TKECOMMERCE/FrmYAHOO.cs
+++ TKECOMMERCE/FrmYAHOO.cs
@@ -129,17 +129,91 @@ namespace TKECOMMERCE
             try
             {
                 DataTable dt = new DataTable();
-                dt.Columns.AddRange(new DataColumn[2] {
+                dt.Columns.AddRange(new DataColumn[30] {
                 new DataColumn("ID", typeof(string)),
-                new DataColumn("NAME", typeof(string))
+                new DataColumn("NAME", typeof(string)),
+                new DataColumn("SERNO", typeof(string)),
+                new DataColumn("PAYKIND", typeof(string)),
+                new DataColumn("RECIVER", typeof(string)),
+                new DataColumn("POST", typeof(string)),
+                new DataColumn("ADDER", typeof(string)),
+                new DataColumn("ADDDATE", typeof(DateTime)),
+                new DataColumn("SHIPDATE", typeof(DateTime)),
+                new DataColumn("SHOPDATE", typeof(DateTime)),
+                new DataColumn("KIND", typeof(string)),
+                new DataColumn("DELIVERY", typeof(string)),
+                new DataColumn("YNO", typeof(string)),
+                new DataColumn("PNO", typeof(string)),
+                new DataColumn("PNAME", typeof(string)),
+                new DataColumn("REMARK", typeof(string)),
+                new DataColumn("SPEC", typeof(string)),
+                new DataColumn("QUANTITY", typeof(string)),
+                new DataColumn("TMONEY", typeof(string)),
+                new DataColumn("STATES", typeof(string)),
+                new DataColumn("INMONEYDATE", typeof(DateTime)),
+                new DataColumn("TELDAY", typeof(string)),
+                new DataColumn("TELNIGHT", typeof(string)),
+                new DataColumn("MOBILE", typeof(string)),
+                new DataColumn("TAX", typeof(string)),
+                new DataColumn("BONUS", typeof(string)),
+                new DataColumn("BONUSMONEY", typeof(string)),
+                new DataColumn("DISCOUNTACT", typeof(string)),
+                new DataColumn("DISCOUNTCODE", typeof(string)),
+                new DataColumn("DISCOUNTMONEY", typeof(string))
             });
 
                 foreach (DataGridViewRow row in dataGridView1.Rows)
                 {
                     string ID = row.Cells["訂單編號"].Value.ToString();
                     string NAME = row.Cells["訂購人"].Value.ToString();
+                    string SERNO = row.Cells["交易序號"].Value.ToString();
+                    string PAYKIND = row.Cells["付款別"].Value.ToString();
+                    string RECIVER = row.Cells["收件人姓名"].Value.ToString();
+                    string POST = row.Cells["收件人郵遞區號"].Value.ToString();
+                    string ADDER = row.Cells["收件人地址"].Value.ToString();
+                    DateTime ADDDATE = Convert.ToDateTime(row.Cells["轉單日期"].Value.ToString());
+                    DateTime SHIPDATE = Convert.ToDateTime(row.Cells["最晚出貨日"].Value.ToString());
+                    DateTime SHOPDATE = new DateTime(1911, 1, 1);
+                    string KIND = row.Cells["商品類型"].Value.ToString();
+                    string DELIVERY = row.Cells["物流設定"].Value.ToString();
+                    string YNO = row.Cells["商品編號"].Value.ToString();
+                    string PNO = row.Cells["店家商品料號"].Value.ToString();
+                    string PNAME = row.Cells["商品名稱"].Value.ToString();
+                    string REMARK = row.Cells["購物車備註"].Value.ToString();
+                    string SPEC = row.Cells["商品規格"].Value.ToString();
+                    string QUANTITY = row.Cells["數量"].Value.ToString();
+                    string TMONEY = row.Cells["金額小計"].Value.ToString();
+                    string STATES = row.Cells["訂單狀態"].Value.ToString();
+                    DateTime INMONEYDATE = new DateTime(1911, 1, 1);
+                    string TELDAY = row.Cells["收件人電話(日)"].Value.ToString();
+                    string TELNIGHT = row.Cells["收件人電話(夜)"].Value.ToString();
+                    string MOBILE = row.Cells["收件人行動電話"].Value.ToString();
+                    string TAX = row.Cells["商品稅別"].Value.ToString();
+                    string BONUS = row.Cells["超贈點點數"].Value.ToString();
+                    string BONUSMONEY = row.Cells["超贈點折抵金額"].Value.ToString();
+                    string DISCOUNTACT = row.Cells["折扣碼活動編號"].Value.ToString();
+                    string DISCOUNTCODE = row.Cells["折扣碼"].Value.ToString();
+                    string DISCOUNTMONEY = row.Cells["折扣碼折抵金額"].Value.ToString();
 
-                    dt.Rows.Add(ID, NAME);
+                    if (string.IsNullOrEmpty(row.Cells["店家出貨日"].Value.ToString()))
+                    {
+                        SHOPDATE = new DateTime(1911, 1, 1);
+                    }
+                    else
+                    {
+                        SHOPDATE = Convert.ToDateTime(row.Cells["店家出貨日"].Value.ToString());
+                    }
+
+                    if (string.IsNullOrEmpty(row.Cells["入帳日"].Value.ToString()))
+                    {
+                        INMONEYDATE = new DateTime(1911, 1, 1);
+                    }
+                    else
+                    {
+                        INMONEYDATE = Convert.ToDateTime(row.Cells["入帳日"].Value.ToString());
+                    }
+
+                    dt.Rows.Add(ID, NAME, SERNO, PAYKIND, RECIVER, POST, ADDER,ADDDATE,SHIPDATE,SHOPDATE, KIND, DELIVERY,YNO, PNO, PNAME,REMARK, SPEC, QUANTITY, TMONEY,STATES,INMONEYDATE,TELDAY,TELNIGHT ,MOBILE, TAX ,BONUS,BONUSMONEY, DISCOUNTACT, DISCOUNTCODE, DISCOUNTMONEY);
                 }
 
                 if (dt.Rows.Count > 0)
@@ -155,6 +229,35 @@ namespace TKECOMMERCE
                             //[OPTIONAL]: Map the DataTable columns with that of the database table
                             sqlBulkCopy.ColumnMappings.Add("ID", "ID");
                             sqlBulkCopy.ColumnMappings.Add("NAME", "NAME");
+                            sqlBulkCopy.ColumnMappings.Add("SERNO", "SERNO");
+                            sqlBulkCopy.ColumnMappings.Add("PAYKIND", "PAYKIND");
+                            sqlBulkCopy.ColumnMappings.Add("RECIVER", "RECIVER");
+                            sqlBulkCopy.ColumnMappings.Add("POST", "POST");
+                            sqlBulkCopy.ColumnMappings.Add("ADDER", "ADDER");
+                            sqlBulkCopy.ColumnMappings.Add("ADDDATE", "ADDDATE");
+                            sqlBulkCopy.ColumnMappings.Add("SHIPDATE", "SHIPDATE");
+                            sqlBulkCopy.ColumnMappings.Add("SHOPDATE", "SHOPDATE");
+                            sqlBulkCopy.ColumnMappings.Add("KIND", "KIND");
+                            sqlBulkCopy.ColumnMappings.Add("DELIVERY", "DELIVERY");
+                            sqlBulkCopy.ColumnMappings.Add("YNO", "YNO");
+                            sqlBulkCopy.ColumnMappings.Add("PNO", "PNO");
+                            sqlBulkCopy.ColumnMappings.Add("PNAME", "PNAME");
+                            sqlBulkCopy.ColumnMappings.Add("REMARK", "REMARK");
+                            sqlBulkCopy.ColumnMappings.Add("SPEC", "SPEC");
+                            sqlBulkCopy.ColumnMappings.Add("QUANTITY", "QUANTITY");
+                            sqlBulkCopy.ColumnMappings.Add("TMONEY", "TMONEY");
+                            sqlBulkCopy.ColumnMappings.Add("STATES", "STATES");
+                            sqlBulkCopy.ColumnMappings.Add("INMONEYDATE", "INMONEYDATE");
+                            sqlBulkCopy.ColumnMappings.Add("TELDAY", "TELDAY");
+                            sqlBulkCopy.ColumnMappings.Add("TELNIGHT", "TELNIGHT");
+                            sqlBulkCopy.ColumnMappings.Add("MOBILE", "MOBILE");
+                            sqlBulkCopy.ColumnMappings.Add("TAX", "TAX");
+                            sqlBulkCopy.ColumnMappings.Add("BONUS", "BONUS");
+                            sqlBulkCopy.ColumnMappings.Add("BONUSMONEY", "BONUSMONEY");
+                            sqlBulkCopy.ColumnMappings.Add("DISCOUNTACT", "DISCOUNTACT");
+                            sqlBulkCopy.ColumnMappings.Add("DISCOUNTCODE", "DISCOUNTCODE");
+                            sqlBulkCopy.ColumnMappings.Add("DISCOUNTMONEY", "DISCOUNTMONEY");
+
 
                             con.Open();
                             sqlBulkCopy.WriteToServer(dt);
